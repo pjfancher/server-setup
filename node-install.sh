040c219b20c1150a9c9fd6d63507447e5200814f
PJ Fancher <peejfancher@gmail.com>
2022-09-26 17:58:27 +0000

    master@dev: Added node-install

---
+++ node-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-22-04#option-3-installing-node-using-the-node-version-manager
+
+NVM_VERSION=0.39.1
+NODE_VERSION=14.20.0
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$NVM_VERSION/install.sh | bash
+source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install v$NODE_VERSION
+node -v
+
+sudo apt install npm -y
+sudo npm install -g npm
+sudo npm install -g yarn
