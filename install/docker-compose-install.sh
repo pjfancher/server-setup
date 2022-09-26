b965d6575a51232f826cc0981a4b7a608c6236e2
PJ Fancher <peejfancher@gmail.com>
2022-09-26 16:37:44 -0400

    Update docker-compose-install.sh

--- install/docker-compose-install.sh
+++ install/docker-compose-install.sh
@@ -3,6 +3,8 @@
 # Install Docker-Compose
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-22-04
 
+printf "Installing docker compose\n"
+
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.11.1/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
