739fb271b1b148007a42c7154dd7fc147a8d2086
PJ Fancher <peejfancher@gmail.com>
2022-09-26 14:12:28 -0400

    Update server-setup-22-04.sh

--- server-setup-22-04.sh
+++ server-setup-22-04.sh
@@ -71,29 +71,6 @@ done
 
 #*****************************************************************************
 #*****************************************************************************
-# Install NVM, Node, NPM, Yarn
-#*****************************************************************************
-sudo mkdir $NVM_DIR
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-sudo $NVM_DIR/nvm-exec \
-	&& nvm install $NODE_VERSION \
-	&& nvm alias default $NODE_VERSION \
-	&& nvm use default
-
-# Update npm
-sudo npm install -g npm
-
-# Install yarn
-sudo npm install --global yarn
-
-
-#*****************************************************************************
-#*****************************************************************************
 # Install Docker
 #*****************************************************************************
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
