05d9643f320f5eef449e9db359b9012811ffbae7
PJ Fancher <peejfancher@gmail.com>
2022-09-26 11:52:24 -0400

    Update server-setup-22-04.sh

--- server-setup-22-04.sh
+++ server-setup-22-04.sh
@@ -75,6 +75,11 @@ done
 #*****************************************************************************
 sudo mkdir $NVM_DIR
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 sudo $NVM_DIR/nvm-exec \
 	&& nvm install $NODE_VERSION \
 	&& nvm alias default $NODE_VERSION \
