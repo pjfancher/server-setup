3f4265c2e5189005540c37a6520e6f728f7851a1
PJ Fancher <peejfancher@gmail.com>
2022-09-26 14:42:39 +0000

    master@careeners: added links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Server Setup Script Ubuntu 22.04 - September 2022
 
 https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-22-04
+https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
+https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-22-04
--- server-setup-22-04.sh
+++ server-setup-22-04.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-##################################################################################################################
-#
-#	Do This First:
-#	https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-22-04
-#
-##################################################################################################################
+# https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-22-04
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-22-04
 
 # USERS
 #*****************************************************************************
